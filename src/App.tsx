d187b7f5a88d437fe8456c78eae0524e6872c287
vincent <kigongovincent81@gmail.com>
2025-03-03 15:10:53 +0300

    worked on the landing screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,24 @@ import NewBuildConfirmation from "./components/NewBuildConfirmation"
 import PurchaseEstimate1 from "./components/PurchaseEstimate1"
 import PurchaseEstimate2 from "./components/PurchaseEstimate2"
 import PurchaseEstimate3 from "./components/PurchaseEstimate3"
-import { Box, Button, createTheme, ThemeProvider } from "@mui/material"
+import { Box, Button, createTheme, Dialog, LinearProgress, ThemeProvider, Typography } from "@mui/material"
 
 const App = () => {
 
   const theme = createTheme({
     defaultColorScheme: "light",
     palette: {
-      primary: {main: "#ffc107"}
+      primary: { main: "#ffc107" },
+    },
+    typography: {
+      fontSize: 13,
+      fontFamily: "poppins"
     }
   })
 
   const [content, setContent] = useState<Content>()
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(true)
   const contents: Content[] = [
     {
       title: "landing",
@@ -39,17 +45,37 @@ const App = () => {
   ]
 
   useEffect(() => {
-    setContent(contents[1])
-    console.log(content)
+    setContent(contents[0])
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000);
   }, [])
+
+
 
 
   return (
     <ThemeProvider theme={theme}>
       <Box display={"flex"} justifyContent={"center"} alignItems={"center"} height={"100vh"} width={"100vw"}>
-        
-      {/* trigger for getting a quote  */}
-      <Button variant="contained">Get a quote</Button>
+
+        {/* trigger for getting a quote  */}
+        <Button onClick={() => setOpen(true)} variant="contained">Get a quote</Button>
+
+        {/* modal for displaying the forms  */}
+        <Dialog open={open} onClose={() => setOpen(false)} fullWidth>
+          {
+            loading
+              ?
+              <Box  display={"flex"} flexDirection={"column"} padding={"20px 0px 0px 0px"} alignItems={"center"} justifyContent={"space-between"}>
+                <Typography variant="h4">Getting things ready</Typography>
+                <Typography variant="caption">please wait as we collect the neccessary resources...</Typography>
+                <br />
+                <LinearProgress style={{ width: "100%" }} />
+              </Box>
+              :
+              content?.body
+          }
+        </Dialog>
 
       </Box>
     </ThemeProvider>
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,6 +1,40 @@
+import { Box, Button, colors,Paper, Typography } from "@mui/material"
+
+export interface Option {
+  label: string
+  action?: () => void
+}
+
 const Landing = () => {
+
+  const options: Option[] = [
+    {
+      label: "Buying a property",
+    },
+    {
+      label: "Selling a property"
+    },
+    {
+      label: "Remortgage",
+    },
+    {
+      label: "Buy and sell a property"
+    }
+  ]
+
   return (
-    <div>Landing</div>
+    <Box display={"flex"}style={{ padding: 20}} flexWrap={"wrap"}>
+      {
+        options?.map((option, index)=>(
+          <Paper elevation={0}  key={index} style={{background: colors?.grey[100], width: "40%", margin: "1%"}} sx={{p: 2, display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
+          <Typography  sx={{mb:1, textTransform: "uppercase"}}>{option?.label}</Typography>
+          <Button disableElevation disabled={index != 0} variant="contained">
+            <Typography color={colors.grey[100]}>get a quote</Typography>
+          </Button>
+        </Paper>
+        ))
+      }
+    </Box>
   )
 }
 
